2da7079e1a1495efa7110c8fb429e86f6707e001
unknown <sheikhrasel35>
2025-09-21 23:38:43 +0600

    added the all files

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Suspense } from 'react'
+import './App.css'
+import Countries from './components/Countries/Countries'
+
+
+const countriesPromise = fetch('https://openapi.programming-hero.com/api/all')
+  .then(res => res.json())
+
+
+function App() {
+  return (
+    <>
+      <Suspense fallback={<p>Nadir vai loading....</p>}>
+        <Countries countriesPromise={countriesPromise}></Countries>
+      </Suspense>
+    </>
+  )
+}
+
+export default App
